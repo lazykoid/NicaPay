b9ffc41269abfbc2f8fe0b1b26bda9e49aac1507
koidfas <koidfas@admin.com>
2025-01-22 08:43:23 -0300

    Proper .env file

--- src/bot.py
+++ src/bot.py
@@ -6,15 +6,17 @@ import os
 # Configurando os intents e a instância do bot
 intents = discord.Intents.default()  
 intents.message_content = True
-discordKey = discord_token = os.getenv("DISCORD-KEY")
+
+# .env
+id = os.getenv("CHAT-ID")
+discordKey = os.getenv("DISCORD-KEY")
 
 # Inicializa o bot com o prefixo '!' e os intents definidos
 bot = commands.Bot(command_prefix='!', intents=intents)  
 
 # Função que é chamada quando o bot está pronto
 @bot.event
-async def on_ready():  
-    id = 1331368645149855754  # ID do canal onde as mensagens de pagamento serão enviadas
+async def on_ready():    
     print(f'Logged on as {bot.user}!')  
     print(f'Uptime: {round(bot.latency * 1000)}ms') 
     await deleteChat(id=id, bot=bot)  
