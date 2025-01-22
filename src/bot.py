6753bdfaaf2736c4694c28eb0686af88ef4803f6
Leonardo Cezar Gauss <koidfas1@gmail.com>
2025-01-22 09:01:43 -0300

    hardcoded chat id

--- src/bot.py
+++ src/bot.py
@@ -8,7 +8,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 # .env
-id = os.getenv("CHAT-ID")
+id = 1331368645149855754
 discordKey = os.getenv("DISCORD-KEY")
 
 # Inicializa o bot com o prefixo '!' e os intents definidos
